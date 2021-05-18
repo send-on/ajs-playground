1e7988fdb43a507178e1487692b90067ed9abb8f
send_on <ohan88@gmail.com>
2021-05-18 10:28:40 -0700

    initial building blocks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { Table, Pane, Textarea, Label } from 'evergreen-ui'
 
-function App() {
+function ControlledTextareaExample() {
+  const [value, setValue] = React.useState('')
+  return <Textarea onChange={(e) => setValue(e.target.value)} value={value} />
+}
+
+function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-left m-8 w-8/12">
+
+      <Table.Body>
+        <Table.Head>
+          <Table.TextCell flexBasis={250} flexShrink={0} flexGrow={0}>
+            Type
+          </Table.TextCell>
+          <Table.TextCell>UserID</Table.TextCell>
+          <Table.TextCell>Body</Table.TextCell>
+        </Table.Head>
+        <Table.Body>
+          <Table.Row height="auto" paddingY={12}>
+            <Table.TextCell flexBasis={250} flexShrink={0} flexGrow={0}>
+              Dropdown here
+            </Table.TextCell>
+            <Table.TextCell>userID</Table.TextCell>
+            <Table.TextCell>
+            <Pane>
+              <Textarea id="textarea-2" placeholder="{ .... }" />
+            </Pane>
+            </Table.TextCell>
+          </Table.Row>
+        </Table.Body>
+      </Table.Body>
+
+      
     </div>
   );
 }
 
 export default App;
+
